15c1db42c2b0170ff41079a01ee15f34ed008330
livioyang123 <145969464+livioyang123@users.noreply.github.com>
2023-11-08 10:45:03 +0100

    Add files via upload

--- src/main/java/com/example/Server.java
+++ src/main/java/com/example/Server.java
@@ -1,9 +1,8 @@
 package com.example;
 
-
-
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
 
 public class Server {
     public  void avvioServer(){
@@ -34,9 +33,46 @@ public class Server {
 
 class ClientHandler implements Runnable {
     private Socket clientSocket;
+    private HashMap<String,Socket> clientList = new HashMap<String,Socket>();
+    private String clientName;
+    private enum messageType{
+        msg1,
+        msg2,
+        msg3
+    }
 
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
+    }
+
+    public void addClient(String clientName){
+        clientList.put(clientName,clientSocket);
+    }
+
+    public boolean verifyClientName(String clientName){
+        return clientList.containsKey(clientName);
+    }
+
+    public void sendMessage_private(String senderName,String receiverName,String Message,String msgType){
+        Socket receiverSocket = clientList.get(receiverName);
+        if(receiverSocket !=null){
+            try {
+                //String typeMsg="msg"+msgType;
+                DataOutputStream outVersoClient = new DataOutputStream(receiverSocket.getOutputStream());
+                outVersoClient.writeBytes(senderName+"*"+msgType+"* "+receiverName+": "+Message+"\n");
+                //*msg 1* nome_dest: messaggio
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+        
+
+    }
+    public void sendMessage_pubblic(String senderName,String receiverName,String Message,String msgType){
+        for(String clientName : clientList.keySet()){
+            if(!clientName.equalsIgnoreCase(senderName))sendMessage_private(senderName, receiverName, Message, msgType);
+        }
     }
     
     @Override
@@ -48,7 +84,20 @@ class ClientHandler implements Runnable {
         ) {
 
             try {
-                    
+                //verifica nome
+                do{
+
+                    outVersoClient.writeBytes("*insert name*inserisci il tuo nome"+"\n");
+                    clientName = inDaClient.readLine();
+
+                }while(verifyClientName(clientName));
+                //
+
+                
+                
+                
+
+
 
             } catch (Exception e) {
                 outVersoClient.writeBytes("ERRORE"+"\n");
