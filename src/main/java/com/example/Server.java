66b4a96932b7bb9097094c5aa117ee783212adea
lai2-24 <lai2-24@itismeucci.com>
2023-11-08 08:32:50 +0100

    2

---
+++ src/main/java/com/example/Server.java
@@ -0,0 +1,62 @@
+package com.example;
+
+
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public  void avvioServer(){
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(6789);
+            System.out.println("Server in attesa di connessioni...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread clientHandlerThread = new Thread(new ClientHandler(clientSocket));
+                clientHandlerThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+    private Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+    
+    @Override
+    public void run(){
+
+        try (
+            BufferedReader inDaClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            DataOutputStream outVersoClient = new DataOutputStream(clientSocket.getOutputStream());
+        ) {
+
+            try {
+                    
+
+            } catch (Exception e) {
+                outVersoClient.writeBytes("Inserisci un numero valido"+"\n");
+            }
+                
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
