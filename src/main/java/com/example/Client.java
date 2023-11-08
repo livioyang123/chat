512c8a6c2717b39b3fe5a3240dca13c83abb9ab2
lai2-24 <lai2-24@itismeucci.com>
2023-11-08 08:27:47 +0100

    1

---
+++ src/main/java/com/example/Client.java
@@ -0,0 +1,51 @@
+package com.example;
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+    String nomeServer = "localhost";
+    int portaServer = 6789;
+    Socket miSocket;
+    BufferedReader tastiera;
+    String strRicevutaDalServer;
+    DataOutputStream outVersoServer;
+    BufferedReader inDaServer;
+
+    String n1;
+    String op;
+    String n2;
+    
+
+    public Socket connetti(){
+        try {
+            tastiera = new BufferedReader(new InputStreamReader(System.in));
+            miSocket = new Socket(nomeServer,portaServer);
+
+            outVersoServer = new DataOutputStream(miSocket.getOutputStream());
+            inDaServer = new BufferedReader(new InputStreamReader(miSocket.getInputStream()));
+
+        } catch (UnknownHostException e) {
+            // TODO: handle exception
+            System.err.println("host sconosciuto");
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("errore durante la connesione ");
+            System.exit(1);
+        }
+        return miSocket;
+    }
+
+    public void comunica(){
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+            System.out.println("errore durante la connesione con il server!");
+            System.exit(1);
+        }
+    }
+    
+}
